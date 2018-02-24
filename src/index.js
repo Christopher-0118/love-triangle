abd620780a6c5baaaa127a5bb32c3c719e8a54f5
Christopher-0118 <k.karantsevich@gmail.com>
2018-02-24 14:57:04 +0300

    love-triangles/feature: implement getLoveTrianglesCount method

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let countOftriangles = 0;
+
+  for (let i = 0; i < preferences.length; i++) {
+    let spichonee = preferences[i];
+    let nextSpichonee = preferences[spichonee - 1];
+    let prevSpichonee = preferences[nextSpichonee - 1];
+    
+    //check repetition of combinations and the link
+    if ( i < spichonee - 1 && i < nextSpichonee - 1 && i === prevSpichonee - 1) {
+      countOftriangles++; 
+    } 
+  }
+  return countOftriangles;
 };
